25577904bf2b55b8b4f03e7d9da42370842ffd6c
unknown <MustafaRazaShaikh>
2024-06-05 20:37:50 +0500

    if task2

---
+++ main.js
@@ -0,0 +1,13 @@
+let num1 = prompt("Enter any 1st integer")
+let num2 = prompt("Enter any 2nd integer")
+
+if(num1 == num2){
+    document.write("Both Integers are equal")
+    console.log("Both Integers are equal");
+}else if(num1 > num2){
+    document.write("The larger integer is Number 1")
+    console.log("The larger integer is Number 1");
+}else{
+     document.write("The smaller integer is Number 2")
+     console.log("The smaller integer is Number 2");
+ }
